b0509777a40b1a78a94fe92d7d959c3c5c924219
daniil tsyrempilov <daniiltsyrempilov003@gmail.com>
2024-02-27 18:10:51 +0700

    add table 'user_products'

--- src/public/post-registrate.php
+++ src/public/post-registrate.php
@@ -7,12 +7,33 @@ function ValidateUser(array $val): array {
 
     if(isset($val['name'])) {
         $name = $val['name'];
+        if(empty($name)) {
+            $errors['name'] = 'Name not be empty';
+        } elseif(strlen($name) < 2) {
+            $errors['name'] = 'The name must have more than 2 characters';
+        }
     } else {
         $errors['name'] = 'Name must be fill';
     }
 
     if(isset($val['email'])) {
         $email = $val['email'];
+
+        if(empty($email)) {
+            $errors['email'] = 'Email not be empty';
+        } elseif(strlen($email) < 2) {
+            $errors['email'] = 'The email must have more than 2 characters';
+        } else {
+            $res = '';
+            for($i = 0; $i < strlen($email); $i++) {
+                if($email[$i] === '@') {
+                    $res = $res.$email[$i];
+                }
+            }
+            if(strlen($res) !== 1) {
+                $errors['email'] = 'Email is wrong';
+            }
+        }
     } else {
         $errors['email'] = 'Email must be fill';
     }
@@ -29,28 +50,6 @@ function ValidateUser(array $val): array {
         $errors['psw-repeat'] = 'Password-repeat must be fill';
     }
 
-    if(empty($name)) {
-        $errors['name'] = 'Name not be empty';
-    } elseif(strlen($name) < 2) {
-        $errors['name'] = 'The name must have more than 2 characters';
-    }
-
-    if(empty($email)) {
-        $errors['email'] = 'Email not be empty';
-    } elseif(strlen($email) < 2) {
-        $errors['email'] = 'The email must have more than 2 characters';
-    } else {
-        $res = '';
-        for($i = 0; $i < strlen($email); $i++) {
-            if($email[$i] === '@') {
-                $res = $res.$email[$i];
-            }
-        }
-        if(strlen($res) !== 1) {
-            $errors['email'] = 'Email is wrong';
-        }
-    }
-
     if(empty($password)) {
         $errors['psw'] = 'Password not be empty';
     } elseif(strlen($password) < 5) {
@@ -58,12 +57,30 @@ function ValidateUser(array $val): array {
     } elseif($password !== $passwordRep) {
         $errors['psw-repeat'] = 'Password does not match';
     }
+
     return $errors;
 }
 
 $errors = ValidateUser($val);
 
-if (empty($errors)) {
+if(empty($errors)) {
+    $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=laravel", "root", "root");
+
+    $email = $_POST['email'];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $stmt->execute(['email' => $email]);
+
+    $user = $stmt->fetch();
+
+    if($user) {
+        $errors['email'] = 'A user with such an email exists';
+        $flag = true;
+    }
+
+}
+
+if ($flag == false) {
     $pdo = new PDO("pgsql:host=postgres; port=5432; dbname=laravel", "root", "root");
 
     $name = $_POST['name'];
@@ -76,7 +93,7 @@ if (empty($errors)) {
     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
     $stmt->execute(['email' => $email]);
 
-    $result = $stmt->fetch();
+    $user = $stmt->fetch();
 
     #print_r($result);
 }
